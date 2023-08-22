1dd8dfc2da02d70992331eb10a19f54eca9f8dee
Damon <jr.way123@gmail.com>
2023-08-22 19:03:21 +0800

    playGround

--- playGround.py
+++ playGround.py
@@ -1,5 +1,13 @@
 import streamlit as st
 import pandas as pd
 
-st.set_page_config(page_title='Sales Dashboard')
-st.sidebar.header('Filter In Here!')
+filePath = 'amazonOrderData/order.csv'
+df = pd.read_csv(filePath)
+keywords = df.columns.tolist()
+
+selectCols = st.multiselect('Select Columns', keywords)
+priceRange = st.slider('Price Range', float(df['item-price'].min()), float(df['item-price'].max()),
+                       (float(df['item-price'].min()), float(df['item-price'].max())))
+
+filteredData = df[selectCols][(df['item-price'] >= priceRange[0]) & (df['item-price'] <= priceRange[1])]
+st.dataframe(filteredData)
